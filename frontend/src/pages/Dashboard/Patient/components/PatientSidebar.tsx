bb73c06ccab3cffba27d7f8c714db69ebc87e10a
Kelvin205 <kelvinbermejo8@gmail.com>
2026-04-11 10:43:01 -0400

    Enhanced Nurse Menu

--- frontend/src/pages/Dashboard/Patient/components/PatientSidebar.tsx
+++ frontend/src/pages/Dashboard/Patient/components/PatientSidebar.tsx
@@ -113,7 +113,7 @@ export default function PatientSidebar() {
   return (
     <Sidebar
       collapsible="icon"
-      className="top-[115px] h-[calc(100vh-500px)] bg-white rounded-2xl text-yellow-600"
+      className="top-[115px] h-[calc(100vh-500px)] bg-white rounded-2xl"
     >
       <SidebarHeader className="border bg-white rounded-xl border-slate-300 px-3 py-4">
         <div className="flex items-start justify-between gap-3 group-data-[collapsible=icon]:justify-center">
